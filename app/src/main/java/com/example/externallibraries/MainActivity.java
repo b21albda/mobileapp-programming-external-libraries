24a660ed8240587f7b09bd27b4ad3526c018d816
Albin Dahlén <b21albda@student.his.se>
2022-05-13 09:38:37 +0200

    Added load image from url and internet permission

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.externallibraries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+        ImageView img = findViewById(R.id.imageView);
+        EditText input = findViewById(R.id.et_input);
+
+        btn.setOnClickListener(View -> {
+            String url = input.getText().toString();
+
+            if (!url.isEmpty())
+                Picasso.get().load(url).into(img);
+        });
     }
 }
